8cad27a29b7adf599a5c1aacddacedf69a994674
Junior Hernandez <junihh@me.com>
2017-06-06 08:06:16 -0400

    Timestamp removed

--- modules/timestamp.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import datetime
-import time
-
-def timestamp():
-    ts = time.time()
-    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-    return st
