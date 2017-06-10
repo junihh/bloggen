9ce4e37a3888f2aa28913c1478bac4ec06ff5015
Junior Hernandez <junihh@me.com>
2017-06-09 20:31:06 -0400

    Writing the Bloggen class

---
+++ modules/gen.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+import os, sys, hashlib, json, base64, mimetypes
+from bs4 import BeautifulSoup
+import mistune
+import yaml
+import jinja2
+from filemimes import filemimes
+
+
+class Bloggen(object):
+    def __init__(self):
+        pass
+
+    def run(self,settings=None):
+        pass
+
+    def makeHTML(self,data=None,output=None):
+        pass
+
+    def filetoB64(self,dirpath=None,sourcepath=None,raw=False):
+        pass
+
+    def encodeSources(self,dirpath=None):
+        pass
+
+    
+
+
+
+
+
+
+
+
+
+
+
